b7105829f97a1489e30ed96803471ac10f6473a1
Zia97 <qasimziauddin@gmail.com>
2020-03-07 17:47:41 +0000

    Base panels

---
+++ UCATAPP/Assets/PracticeQuestionControllerGenericScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PracticeQuestionControllerGenericScript : MonoBehaviour
+{
+
+    public Text HeaderPanelText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HeaderPanelText.text = GlobalVariables.SelectedPracticeQuestion;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- UCATAPP/Assets/Scripts/SceneLoader.cs
+++ UCATAPP/Assets/Scripts/SceneLoader.cs
@@ -10,4 +10,9 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void SetSelectedPracticeQuestion(string selectedQuestion)
+    {
+        GlobalVariables.SelectedPracticeQuestion = selectedQuestion;
+    }
 }
