f9e5c1bc7aca0904675bb3a46788d4ba769b83f6
Zia97 <qasimziauddin@gmail.com>
2020-03-16 21:23:35 +0000

    Loading jsons

--- UCATAPP/Assets/PracticeQuestionControllerGenericScript.cs
+++ UCATAPP/Assets/PracticeQuestionControllerGenericScript.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,15 +7,48 @@ public class PracticeQuestionControllerGenericScript : MonoBehaviour
 {
 
     public Text HeaderPanelText;
+    public TextAsset jsonFile;
+
     // Start is called before the first frame update
     void Start()
     {
         HeaderPanelText.text = GlobalVariables.SelectedPracticeQuestion;
+
+        if(GlobalVariables.SelectedPracticeQuestion.Equals("Abstract Reasoning"))
+        {
+            Debug.Log("Loading practice question");                   
+        }
+
+        string json = (File.ReadAllText(Application.dataPath + "\\PracticeQuestionJSONS\\AbstractReasoning\\AbstractReasoningQuestions.json"));
+        AllQuestions allQuestions = JsonUtility.FromJson<AllQuestions>(json);
+
+        //Examples to show structure
+        foreach (Question q in allQuestions.allQuestions)
+        {
+            Debug.Log(q.questionType);
+        }
+
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+}
+
+[System.Serializable]
+public class Question
+{
+    public string questionType;
+    public string resource;
+    public string[] answers;
+}
+
+[System.Serializable]
+public class AllQuestions
+{
+    public List<Question> allQuestions;
 }
